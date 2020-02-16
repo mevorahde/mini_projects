e571d3cb75fd335fddc262b82d9cf5f1c6e1efc6
David Mevorah <mevorahde@hotmail.com>
2020-02-16 12:22:53 -0800

    Update and complete the NFL Fan Generator UI program.

--- nfl_fan_generator/nfl_fan_generator_ui.py
+++ nfl_fan_generator/nfl_fan_generator_ui.py
@@ -1,11 +1,15 @@
 import tkinter as tk
 from tkinter.font import Font
-from PIL import ImageTk, Image
+from tkinter import *
+import tkinter
+import tkinter.messagebox
+from PIL import Image
+from PIL import ImageTk
 import random
 
 # A dictionary of number 1-32 for the key and the values are a second dictionary for team information: name and logo.
 nfl_teams = {
-    1: {'Name': 'Arizona Cardinals', 'Logo': 'Arizona-Cardinals-logo-vector.jpg'},
+    1: {'Name': 'Arizona Cardinals', 'Logo': 'arizona-cardinals-logo.png'},
     2: {'Name': 'Chicago Bears', 'Logo': 'chicago-bears-logo-vector.png'},
     3: {'Name': 'Green Bay Packers', 'Logo': 'green-bay-packers-logo-vector.png'},
     4: {'Name': 'New York Giants', 'Logo': 'new-york-giants-logo-vector.png'},
@@ -15,26 +19,26 @@ nfl_teams = {
     8: {'Name': 'Pittsburgh Steelers', 'Logo': 'pittsburgh-steelers-logo-vector-01.png'},
     9: {'Name': 'Los Angeles Rams', 'Logo': 'st-louis-rams-vector-logo.png'},
     10: {'Name': 'San Francisco 49ers', 'Logo': 'san-francisco-49ers-logo-vector.png'},
-    11: {'Name': 'Cleveland Browns', 'Logo': 'cleveland-browns-logo-vector-download.jpg'},
-    12: {'Name': 'Indianapolis Colts', 'Logo': 'indianapolis-colts-logo-vector-download.jpg'},
+    11: {'Name': 'Cleveland Browns', 'Logo': 'cleveland-browns-logo.png'},
+    12: {'Name': 'Indianapolis Colts', 'Logo': 'indianapolis-colts-logo.png'},
     13: {'Name': 'Dallas Cowboys', 'Logo': 'dallas-cowboys-logo-vector.png'},
-    14: {'Name': 'Kansas City Chiefs', 'Logo': 'kansas-city-chiefs-logo-vector-download.jpg'},
+    14: {'Name': 'Kansas City Chiefs', 'Logo': 'kansas-city-chiefs-logo.png'},
     15: {'Name': 'Los Angeles Chargers', 'Logo': 'san-diego-chargers-logo-vector.png'},
     16: {'Name': 'Denver Broncos', 'Logo': 'denver-broncos-logo-vector.png'},
     17: {'Name': 'New York Jets', 'Logo': 'new-york-jets-logo-vector-01.png'},
     18: {'Name': 'New England Patriots', 'Logo': 'new-england-patriots-logo-preview.png'},
     19: {'Name': 'Oakland Raiders', 'Logo': 'oakland-raiders-logo-vector.png'},
-    20: {'Name': 'Tennessee Titans', 'Logo': 'tennessee-titans-logo-vector-download.jpg'},
+    20: {'Name': 'Tennessee Titans', 'Logo': 'tennessee-titans-logo.png'},
     21: {'Name': 'Buffalo Bills', 'Logo': 'buffalo-bills-logo-vector-01.png'},
     22: {'Name': 'Minnesota Vikings', 'Logo': 'minnesota-vikings-logo-vector.png'},
     23: {'Name': 'Atlanta Falcons', 'Logo': 'atlanta-falcons-logo-vector.png'},
     24: {'Name': 'Miami Dolphins', 'Logo': 'miami-dolphins-vector-logo.png'},
     25: {'Name': 'New Orleans Saints', 'Logo': 'new-orleans-saints-logo-vector.png'},
-    26: {'Name': 'Cincinnati Bengals', 'Logo': 'cincinnati-bengals-logo-vector-01.png'},
+    26: {'Name': 'Cincinnati Bengals', 'Logo': 'cincinnati-bengals-tiger-logo.png'},
     27: {'Name': 'Seattle Seahawks', 'Logo': 'seattle-seahawks-logo-vector.png'},
     28: {'Name': 'Tampa Bay Buccaneers', 'Logo': 'tampa-bay-buccaneers-logo-vector.png'},
     29: {'Name': 'Carolina Panthers', 'Logo': 'carolina-panthers-logo-vector.png'},
-    30: {'Name': 'Jacksonville Jaguars', 'Logo': 'jacksonville-jaguars-logo-vector-download.jpg'},
+    30: {'Name': 'Jacksonville Jaguars', 'Logo': 'jacksonville-jaguars-logo.png'},
     31: {'Name': 'Baltimore Ravens', 'Logo': 'baltimore-ravens-logo-vector-01.png'},
     32: {'Name': 'Houston Texans', 'Logo': 'houston-texans-logo-vector.png'}
 }
@@ -62,11 +66,14 @@ def team():
 
 
 def load_image(img):
+    width = 325
+    height = 325
     # load the image and display it
     img = Image.open(img)
+    img = img.resize((width, height), Image.ANTIALIAS)
     img = ImageTk.PhotoImage(img)
-    panel.img = img  # keep a reference so it's not garbage collected
-    panel['image'] = img
+    team_logo.img = img  # keep a reference so it's not garbage collected
+    team_logo['image'] = img
 
 
 def start_img():
@@ -90,10 +97,16 @@ def next_img():
 
 
 win = tk.Tk()
-win.geometry('1000x1000')  # set window size
+win.geometry('480x480')  # set window size
 win.resizable(0, 0)  # fix window
-panel = tk.Label(win)
-panel.pack()
+f1 = tk.Frame(win, bg="white", height=350, width=350)
+f1.pack(pady=25)
+team_logo = tk.Label(f1)
+team_logo.lift()
+team_logo.pack()
+
+
+# panel.pack()
 # Main Font
 helv36 = Font(family='Helvetica', size=42, weight='bold')
 # Button for the user to start/continue the program
